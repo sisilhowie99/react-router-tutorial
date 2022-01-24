112b82b59b17c5ac0177418688096e1046f68f5d
sisilhowie99 <sisil.anastasia@gmail.com>
2022-01-24 23:41:24 +1000

    Create basic routes to 2 files

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { BrowserRouter } from 'react-router-dom';
-import App from "./App";
-import "./main.css";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Expenses from './routes/expenses';
+import Invoices from './routes/invoices';
+import './main.css';
 
-let rootElement = document.getElementById("root");
+let rootElement = document.getElementById('root');
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, rootElement);
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='invoices' element={<Invoices />} />
+      <Route path='expenses' element={<Expenses />} />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
+);
