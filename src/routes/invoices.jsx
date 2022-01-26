6d8ec5123006fe2e477aad976c6ed914fe961aa7
sisilhowie99 <sisil.anastasia@gmail.com>
2022-01-27 00:34:31 +1000

    Implement search params to filter invoices

--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,27 +1,43 @@
-import { NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Outlet, useSearchParams } from 'react-router-dom';
+import { setTokenSourceMapRange } from 'typescript/lib/tsserverlibrary';
 import { getInvoices } from '../data';
 
 export default function Invoices() {
   let invoices = getInvoices();
+  let [searchParams, setSearchParams] = useSearchParams();
 
   return (
     <div style={{ display: 'flex' }}>
       <nav style={{ borderRight: '1px solid', padding: '1rem' }}>
-        {invoices.map((invoice) => (
-          <NavLink
-            style={({ isActive }) => {
-              return {
-                display: 'block',
-                margin: '1rem 0',
-                color: isActive ? 'red' : '',
-              };
-            }}
-            to={`/invoices/${invoice.number}`}
-            key={invoice.number}
-          >
-            {invoice.name}
-          </NavLink>
-        ))}
+        <input
+          value={searchParams.get('filter') || ''}
+          onChange={(event) => {
+            let filter = event.target.value;
+            filter ? setSearchParams({ filter }) : setSearchParams({});
+          }}
+        />
+        {invoices
+          .filter((invoice) => {
+            let filter = searchParams.get('filter');
+            if (!filter) return true;
+            let name = invoice.name.toLowerCase();
+            return name.startsWith(filter.toLowerCase());
+          })
+          .map((invoice) => (
+            <NavLink
+              style={({ isActive }) => {
+                return {
+                  display: 'block',
+                  margin: '1rem 0',
+                  color: isActive ? 'red' : '',
+                };
+              }}
+              to={`/invoices/${invoice.number}`}
+              key={invoice.number}
+            >
+              {invoice.name}
+            </NavLink>
+          ))}
       </nav>
       <main style={{ padding: '1rem 0' }}>
         <Outlet />
