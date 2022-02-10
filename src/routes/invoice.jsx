82629b5c90e40626593a46b523d5ba76c5231bcb
sisilhowie99 <sisil.anastasia@gmail.com>
2022-02-10 23:49:23 +1000

    Navigate programmatically done

--- src/routes/invoice.jsx
+++ src/routes/invoice.jsx
@@ -1,18 +1,31 @@
-import { useParams } from 'react-router-dom';
-import { getInvoice } from '../data';
+import { useParams, useNavigate } from 'react-router-dom';
+import { getInvoice, deleteInvoice } from '../data';
 
 export default function Invoice() {
+  let navigate = useNavigate();
   let params = useParams();
   let invoice = getInvoice(parseInt(params.invoiceId, 10));
 
   return (
-    <div>
-      <h2>Invoice {params.invoiceId}</h2>
-      <h3>Total due: {invoice.amount}</h3>
-      <p>
-        {invoice.name}: {invoice.number}
-      </p>
-      <p>Due by: {invoice.due}</p>
-    </div>
+    <main style={{ padding: '1rem' }}>
+      <div>
+        <h2>Invoice {params.invoiceId}</h2>
+        <h3>Total due: {invoice.amount}</h3>
+        <p>
+          {invoice.name}: {invoice.number}
+        </p>
+        <p>Due by: {invoice.due}</p>
+        <p>
+          <button
+            onClick={() => {
+              deleteInvoice(invoice.number);
+              navigate('/invoices');
+            }}
+          >
+            Delete
+          </button>
+        </p>
+      </div>
+    </main>
   );
 }
